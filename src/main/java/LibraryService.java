f95f4ac7cd957262a5651cf1cb0b2940b9944472
Sneha Mirajkar <sneha.miraj009@gmail.com>
2021-08-14 16:01:20 +0200

    add scenario - borrow book from library

--- src/main/java/LibraryService.java
+++ src/main/java/LibraryService.java
@@ -3,14 +3,17 @@ import java.util.List;
 
 public class LibraryService {
 
-    private final List<Book> borrowedBooks = new ArrayList<>();
+    private final List<Book> borrowedBooks = new ArrayList<>(2);
     private final Library library;
 
     public LibraryService(Library library) {
         this.library = library;
     }
 
-    public List<Book> borrow(Book book) {
+    public List<Book> borrow(Book book) throws Exception {
+        if (borrowedBooks.size() == 2) {
+           throw new Exception("can not borrow more than 2 books");
+        }
         borrowedBooks.add(book);
         library.remove(book);
         return borrowedBooks;
--- src/test/java/LibraryServiceTest.java
+++ src/test/java/LibraryServiceTest.java
@@ -3,13 +3,12 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class LibraryServiceTest {
 
     @Test
-    void shouldBorrowABookFromLibrary() {
+    void shouldBorrowABookFromLibrary() throws Exception {
         //given
         List<Book> books = new ArrayList<>();
         Book book = new Book();
@@ -23,5 +22,27 @@ public class LibraryServiceTest {
         //then
         assertFalse(borrowedBooks.isEmpty());
         assertTrue(library.getBooks().isEmpty());
+    }
+
+    @Test
+    void shouldNotBorrowMoreThanTwoBooksFromLibrary() throws Exception {
+        //given
+        List<Book> books = new ArrayList<>();
+        Book book1 = new Book();
+        books.add(book1);
+        Book book2 = new Book();
+        books.add(book2);
+        Book book3 = new Book();
+        books.add(book3);
+        Library library = new Library(books);
+
+        //when
+        LibraryService libraryService = new LibraryService(library);
+        libraryService.borrow(book1);
+        libraryService.borrow(book2);
+        Exception exception = assertThrows(Exception.class, () -> libraryService.borrow(book3));
+
+        //then
+        assertEquals("can not borrow more than 2 books", exception.getMessage());
     }
 }
