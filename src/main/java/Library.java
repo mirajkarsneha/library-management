ec7b92fa84958657039a7b7bb9bc947d745df803
Sneha Mirajkar <sneha.miraj009@gmail.com>
2021-08-14 14:43:45 +0200

    add scenario - list of books in library

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -1,9 +1,14 @@
-import java.util.ArrayList;
 import java.util.List;
 
 public class Library {
 
+    private final List<Book> books;
+
+    public Library(List<Book> books) {
+        this.books = books;
+    }
+
     public List<Book> getBooks() {
-        return new ArrayList<>();
+        return books;
     }
 }
--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -1,7 +1,10 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import static java.util.Collections.emptyList;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class LibraryTest {
@@ -9,12 +12,26 @@ public class LibraryTest {
     @Test
     void shouldGetNoBooksInEmptyLibrary() {
         //given
-        Library library = new Library();
+        Library library = new Library(emptyList());
 
         //when
         List<Book> books = library.getBooks();
 
         //then
         assertTrue(books.isEmpty());
+    }
+
+    @Test
+    void shouldGetBooksInLibrary() {
+        //given
+        List<Book> books = new ArrayList<>();
+        books.add(new Book());
+        Library library = new Library(books);
+
+        //when
+        List<Book> libraryBooks = library.getBooks();
+
+        //then
+        assertFalse(libraryBooks.isEmpty());
     }
 }
